13d5312f579e420c580519f30881b70633110884
Yoshihiro Tanaka <contact@cordea.jp>
2022-09-17 14:41:36 +0900

    define constants

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,37 @@
 module Main where
 
+table :: [[Int]]
+table =
+    [
+        [0, 0, 0, 1, 1],
+        [0, 0, 1, 0, 1],
+        [0, 0, 1, 1, 0],
+        [0, 1, 0, 0, 1],
+        [0, 1, 0, 1, 0],
+        [0, 1, 1, 0, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 1, 0],
+        [1, 0, 1, 0, 0],
+        [1, 1, 0, 0, 0]
+    ]
+
+barHeight :: Int
+barHeight = 23
+
+halfBarHeight :: Int
+halfBarHeight = 8
+
+barWidth :: Int
+barWidth = 3
+
+verticalPadding :: Int
+verticalPadding = 2
+
+horizontalPadding :: Int
+horizontalPadding = 2
+
+quietZone :: Int
+quietZone = 25 - horizontalPadding
+
 main :: IO ()
 main = putStrLn "Hello, Haskell!"
